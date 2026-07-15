338628a8a2d022e0e2c02674ba18f752e04a4023
Illia Kolmykov <zabyz1337@gmail.com>
2026-07-15 16:43:19 +0200

    Configure API client

---
+++ src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  timeout: 4000,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      if (!location.pathname.startsWith("/login")) location.assign("/login");
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
